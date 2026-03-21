75afc6ff32a98a147ecd9a62c97c3b38bff59a11
WaterIris <179727984+WaterIris@users.noreply.github.com>
2026-03-21 09:27:32 +0100

    backup

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,8 +1,8 @@
 local function enable_transparency()
     vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
     vim.api.nvim_set_hl(0, "StatusLine", {
-	fg = "#EB6F92",
-	bg = "#2E202F",
+	fg = "#f7768e",
+	bg = "#2F212F",
 	bold = true,
     })
 
@@ -11,21 +11,43 @@ local function enable_transparency()
 	bg = "#1f1d2e",
     })
 end
+-- return {
+--     {
+-- 	"rose-pine/neovim",
+-- 	config = function()
+-- 	    require("rose-pine").setup({
+--                 styles = {
+--                     italic = false, -- This disables italics globally for the theme
+--                 },
+-- 		-- highlight_groups = {
+-- 		--             StatusLine = { fg = "#7aa2f7", bg = "#7aa2f7", blend = 10 },
+-- 		--             StatusLineNC = { fg = "subtle", bg = "surface" },
+-- 		-- },
+--             })
+-- 	    vim.cmd.colorscheme "rose-pine"
+-- 	    -- enable_transparency()
+-- 	end
+--     },
+-- }
 return {
     {
-	"rose-pine/neovim",
-	config = function()
-	    require("rose-pine").setup({
+        "folke/tokyonight.nvim",
+        lazy = false,
+        -- priority = 1000,
+        config = function()
+            require("tokyonight").setup({
+                style = "night", -- Choose between "storm", "moon", "night", or "day"
+                -- transparent = true, -- Tokyo Night has built-in transparency support
                 styles = {
-                    italic = false, -- This disables italics globally for the theme
+                    -- sidebars = "transparent",
+                    -- floats = "transparent",
+                    keywords = { italic = false }, -- Disables italics as per your preference
                 },
-		-- highlight_groups = {
-		            -- StatusLine = { fg = "#31748f", bg = "#31748f", blend = 10 },
-		            -- StatusLineNC = { fg = "subtle", bg = "surface" },
-		-- },
             })
-	    vim.cmd.colorscheme "rose-pine"
-	    enable_transparency()
-	end
+
+            vim.cmd.colorscheme "tokyonight"
+            -- Apply your custom overrides
+            enable_transparency()
+        end,
     },
 }
