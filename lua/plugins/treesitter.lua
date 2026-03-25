65f63a69269936f7f1e56e9eac5d44b04ed3b362
WaterIris <179727984+WaterIris@users.noreply.github.com>
2026-03-26 00:08:20 +0100

    (update) treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,33 +1,33 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    lazy = false,
+    event = { "BufReadPre", "BufNewFile" },
     build = ":TSUpdate",
-    opts = {
-	ensure_installed = {
-	    "cpp",
-	    "css",
-	    "python",
-	    "nix",
-	},
-	auto_install = false,
-
-	highlight = {
-	    enable = true,
-	    additional_vim_regex_highlighting = false,
-	},
-	indent = {
-	    enable = true,
-	},
-	fold = {
-	    enable = true,
-	},
-    },
     config = function()
-	vim.api.nvim_create_autocmd("FileType", {
-	    callback = function()
-		pcall(vim.treesitter.start)
-	    end,
-	})
-    end
-}
+        -- import nvim-treesitter plugin
+        local treesitter = require("nvim-treesitter")
+        -- configure treesitter
+        treesitter.setup({ -- enable syntax highlighting
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            -- enable indentation
+            indent = { enable = true },
 
+            -- ensure these languages parsers are installed
+            ensure_installed = {
+            },
+            "cpp",
+            "css",
+            "python",
+            "nix",
+        })
+        -- force start treesitter for all filetypes
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = '*',
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
+        })
+    end,
+}
