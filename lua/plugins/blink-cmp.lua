35f151fbea7537d8b3f6828f1972fa6bc9024130
WaterIris <179727984+WaterIris@users.noreply.github.com>
2026-03-30 20:46:05 +0200

    completion fixes

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -18,9 +18,10 @@ return {
         cmdline = {
             keymap = {
                 preset = 'none',
-                ["<CR>"] = { "fallback" },
+                ["<CR>"] = { "accept", "fallback" },
                 ["<Up>"] = { "select_prev", "fallback" },
                 ["<Down>"] = { "select_next", "fallback" },
+                ["<Tab>"] = { "select_next" },
             },
             completion = { menu = { auto_show = true } },
         },
@@ -37,6 +38,7 @@ return {
             },
             menu = {
                 border = "rounded",
+                scrollbar = false,
             },
             documentation = {
                 auto_show = false,
@@ -46,8 +48,16 @@ return {
 
         sources = {
             default = { 'lsp', 'path', 'snippets', 'buffer' },
+            providers = {
+                cmdline = {
+                    min_keyword_length = function(ctx)
+                        -- when typing a command, only show when the keyword is 3 characters or longer
+                        if ctx.mode == 'cmdline' and string.find(ctx.line, ' ') == nil then return 3 end
+                        return 0
+                    end
+                }
+            }
         },
-
         fuzzy = { implementation = "prefer_rust_with_warning" }
     },
     opts_extend = { "sources.default" }
